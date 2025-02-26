7f8b61c7cc99841c00c9a083e890846aea57452a
Tamil Selvi j <140626139+TamilSelvi2003@users.noreply.github.com>
2025-02-26 16:04:28 +0530

    Update ApiEndpoint.js

--- client/src/services/ApiEndpoint.js
+++ client/src/services/ApiEndpoint.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL:'https://localhost:4000',
+  baseURL:'https://dynamic-role-based-access-routes.onrender.com',
     headers:{
         'Content-Type': 'application/json'
     },
