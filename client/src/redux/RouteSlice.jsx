b54d1df1b0113ca0e0d389761b13d662469c41f4
Tamil Selvi j <140626139+TamilSelvi2003@users.noreply.github.com>
2025-03-14 13:28:56 +0530

    Update RouteSlice.jsx

--- client/src/redux/RouteSlice.jsx
+++ client/src/redux/RouteSlice.jsx
@@ -1,48 +1,4 @@
-// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from "axios";
 
-// // Async thunk to fetch routes based on user role
-// export const fetchRoutes = createAsyncThunk(
-//   "routes/fetchRoutes",
-//   async (role, { rejectWithValue }) => {
-//     try {
-//       const response = await axios.get(`http://localhost:4000/api/roles?role=${role}`);
-//       console.log("Fetched Routes:", response.data);
-
-//       // Ensure roleData contains the correct routes
-//       const roleData = response.data.find((r) => r.role === role);
-//       return roleData ? roleData.routes : [];
-//     } catch (error) {
-//       return rejectWithValue(error.response?.data || "Error fetching roles");
-//     }
-//   }
-// );
-
-// const routeSlice = createSlice({
-//   name: "route",
-//   initialState: {
-//     routes: [],
-//     loading: false,
-//     error: null,
-//   },
-//   reducers: {},
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(fetchRoutes.pending, (state) => {
-//         state.loading = true;
-//       })
-//       .addCase(fetchRoutes.fulfilled, (state, action) => {
-//         state.loading = false;
-//         state.routes = action.payload; // Store routes in state
-//       })
-//       .addCase(fetchRoutes.rejected, (state, action) => {
-//         state.loading = false;
-//         state.error = action.payload;
-//       });
-//   },
-// });
-
-// export default routeSlice.reducer;
 
 
 
@@ -55,7 +11,7 @@ export const fetchRoutes = createAsyncThunk(
   "routes/fetchRoutes",
   async (role, { rejectWithValue }) => {
     try {
-      const response = await axios.get(`http://localhost:4000/api/roles?role=${role}`);
+      const response = await axios.get(`https://dynamic-role-based-access-routes.onrender.com/api/roles?role=${role}`);
       console.log("Fetched Routes:", response.data);
 
       // Find the user's role in the response and get assigned routes
